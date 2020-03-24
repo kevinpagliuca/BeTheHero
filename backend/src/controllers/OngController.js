b5f4f812dc6be5bdf0930f9e0a186ec8d4ab1539
Kevin Pagliuca <kevinpagliuca@live.com>
2020-03-24 13:13:21 -0300

    Back-end da aplicação

---
+++ backend/src/controllers/OngController.js
@@ -0,0 +1,37 @@
+const crypto = require('crypto');
+const connection = require('../database/connection');
+
+module.exports = {
+
+    async index(request, response) {
+        const ongs = await connection('ongs').select('*');
+        return response.json(ongs);
+    },
+
+
+    async create(request, response) {
+        const { name, email, whatsapp, city, uf } = request.body;
+
+        const id = crypto.randomBytes(4).toString('HEX');
+
+        await connection('ongs').insert({
+            id,
+            name,
+            email,
+            whatsapp,
+            city,
+            uf,
+        })
+        return response.json({ id });
+    },
+
+
+    async delete(request, response) {
+        const { name } = request.params;
+
+        const Ong = await connection('ongs')
+            .where('name', name)
+            .delete();
+        return response.status(204).send();
+    }
+};
---
+++ backend/src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(routes);
+app.listen(3333);
+
+
+/*
+GET: Buscar/listar uma informação do backend
+POST: Criar uma informação do backend
+PUT: Alterar uma informação do backend
+DELETE: Deletar uma informação do backend
+*/
+
+
+/**
+ * Tipos de parâmetros
+ *  
+ * Query Params: Parametros nomeados enviados na rota apos o simbolo "?" (filtros, paginação)
+ * Route Params: Parametros utilizados para identificar recursos
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos.
+
+ */
